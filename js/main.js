3025e5532a384d94d01720b40ea681e63597a9c9
Pam Gregoretti <prg5rv@virginia.edu>
2016-09-06 10:42:20 -0400

    Scale instagram images
    scale down instafeed images

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
         userId: '8331094',
         clientId: '3744bbfa26b9425980bc510a4fabac24',
         accessToken: '8331094.1677ed0.ed108eaedb3e4697ab59238389076078',
-        template: '<a href="{{link}}"><img src="{{image}}" class="img-thumbnail" width="190" height="190" target="_blank"></a>',
+        template: '<a href="{{link}}"><img src="{{image}}" class="img-thumbnail" width="190" height="auto" target="_blank" max-width="100%"></a>',
 		limit: '20',
         filter: function(image) {
             return image.tags.indexOf('amslerknives') >= 0;
