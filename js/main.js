ebbf48fac8ac0939c73d6078a452748b4c4f6b3e
Pam Gregoretti <prg5rv@virginia.edu>
2016-09-06 10:18:40 -0400

    website
    upload files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+    var feed = new Instafeed({
+        get: 'user',
+        userId: '8331094',
+        clientId: '3744bbfa26b9425980bc510a4fabac24',
+        accessToken: '8331094.1677ed0.ed108eaedb3e4697ab59238389076078',
+        template: '<a href="{{link}}"><img src="{{image}}" class="img-thumbnail" width="190" height="190" target="_blank"></a>',
+		limit: '20',
+        filter: function(image) {
+            return image.tags.indexOf('amslerknives') >= 0;
+        }
+    });
+    feed.run();
